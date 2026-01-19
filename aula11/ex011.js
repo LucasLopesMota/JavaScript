b777cb0187dac13127ce3ac42168d8e8ed496f1b
Lucas Mota <mota.lucas2014@gmail.com>
2026-01-19 20:28:10 -0300

    Novas aulas

---
+++ aula11/ex011.js
@@ -0,0 +1,6 @@
+var vel = 20
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if (vel > 60) { // Condição Simples!
+    console.log('Você ultrapassou a velocidade permitida. MULTADO!')
+}
+console.log('Digija sempre com cinto de segurança!')
